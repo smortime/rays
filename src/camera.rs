eaa18ce790a673c3468d6c7862b684e812f5ca37
Schuyler Mortimer <schuymortimer@gmail.com>
2024-11-24 13:07:47 -0800

    ch 9.3 fixing shadow acne

--- src/camera.rs
+++ src/camera.rs
@@ -121,7 +121,7 @@ impl Camera {
         if depth < 0 {
             return Color::origin();
         }
-        if let Some(rec) = world.hit(r, &Interval::new(0.0, f64::INFINITY)) {
+        if let Some(rec) = world.hit(r, &Interval::new(0.001, f64::INFINITY)) {
             let direction = Vec3::random_on_hemisphere(&rec.normal);
             return 0.5 * Self::ray_color(&Ray::new(rec.p, direction), world, depth - 1);
         }
